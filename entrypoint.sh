905bdf6b192a9c5a789bfc65319c71a2eec2f2d0
audiojak <john.knndy@gmail.com>
2016-04-30 19:43:35 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 mkdir -p /home/git/.ssh
-
 echo $SSH_PUBLIC_KEY > /home/git/.ssh/authorized_keys
-
+chown -R git /home/git/.ssh
 chmod 700 /home/git/.ssh
 chmod 600 /home/git/.ssh/authorized_keys
 
