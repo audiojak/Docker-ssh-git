fbea00ed65f2f843ceeabe4da014fb9461056b9e
audiojak <john.knndy@gmail.com>
2016-04-30 15:42:40 -0400

    permissions

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,9 @@
 
 mkdir -p /home/git/.ssh
 
-echo $SSH_PUBLIC_KEY > /git/.ssh/authorized_keys
+echo $SSH_PUBLIC_KEY > /home/git/.ssh/authorized_keys
+
+chmod 700 /home/git/.ssh
+chmod 600 /home/git/.ssh/authorized_keys
 
 exec /usr/sbin/sshd -D
